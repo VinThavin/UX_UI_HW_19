a8c88a4ec78fbc5002dfcf17d2560b237413c97f
VinThavin <99162333+VinThavin@users.noreply.github.com>
2022-07-14 18:19:31 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log ("Hello Wolrd");
+
+
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+  setTimeout(()=>{
+    splash.classList.add('display-none');
+  },2000);
+})
+
+mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+  
+}
+
